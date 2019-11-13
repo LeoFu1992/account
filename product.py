ec7906e20e6082ff371146c7926e847dc9219dff
Leo Fu <leo510032000@gmail.com>
2019-11-13 20:21:16 +0800

    add isfile function

--- product.py
+++ product.py
@@ -1,13 +1,17 @@
 #記帳程式
 q_sum = []
-with open('product.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #如果出現'商品,價格'就跳過此迴圈
-		name, price = line.strip().split(',') #先將line裡面的分行符號去掉，在用split以 ,為界做切割
-		q_sum.append([name, price])
-#print(q_sum)
-
+import os #operating system
+if os.path.isfile('product.csv'): #檢查檔案在不在
+	print('找到檔案')
+	with open('product.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #如果出現'商品,價格'就跳過此迴圈
+			name, price = line.strip().split(',') #先將line裡面的分行符號去掉，在用split以 ,為界做切割
+			q_sum.append([name, price,])
+	#print(q_sum)
+else:
+	print('檔案已遺失....')
 products = []
 p_sum = []
 while True:
@@ -21,15 +25,13 @@ while True:
 	q_sum.append(q)
 	p_sum.append(int(price))
 #print(products)
-
 for product in products:
 	print(product[0], '的價格是', product[1])
-
+	
 count = 0
 for i in p_sum:
 	count = count + i
 print('今天一共消費金額為', count, '元')
-
 
 with open('product.csv', 'w', encoding='utf-8') as f:#encoding='utf-8'為使用utf-8編碼
 	f.write('商品,價格\n')#新增欄位名稱
